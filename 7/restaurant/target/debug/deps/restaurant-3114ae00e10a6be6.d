4965084df2288924d79e4734791dbe0fb46d71ea
shungo0222 <kimura.shungo@gmail.com>
2022-11-30 08:59:27 +0900

    Study "Rust's Module System Explained!"

---
+++ 7/restaurant/target/debug/deps/restaurant-3114ae00e10a6be6.d
@@ -0,0 +1,5 @@
+/Users/shungo/Programming/Rust/the-rust-lang-book/7/restaurant/target/debug/deps/restaurant-3114ae00e10a6be6.rmeta: src/lib.rs
+
+/Users/shungo/Programming/Rust/the-rust-lang-book/7/restaurant/target/debug/deps/restaurant-3114ae00e10a6be6.d: src/lib.rs
+
+src/lib.rs:
